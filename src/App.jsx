565ccddd4146984bfa5b4efdc4126d39e6b28e9c
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-04-13 14:35:52 -0300

    -- Custom Hook --

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,10 @@
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
-import { useState, useEffect } from "react";
-import { db } from "./data/db";
+import useCart from "./hooks/useCart"
 const App = () => {
 
-    const [data] = useState(db);
-    const [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart")) ?? [])
-
-    useEffect(() => {
-        localStorage.setItem("cart", JSON.stringify(cart))
-    }, [cart])
-
-    function addToCart(newItem) {
-
-        const itemExists = cart.findIndex(cartItem => cartItem.id === newItem.id)
-
-        if (itemExists >= 0) {
-            const newCart = [...cart]
-            newCart[itemExists].quantity += 1
-            setCart(newCart)
-        } else {
-            newItem.quantity = 1
-            setCart([...cart, newItem])
-        }
-    }
-
-    function deleteItem(itemId) {
-        const newCart = cart.filter(cartItem => cartItem.id !== itemId)
-        setCart(newCart)
-    }
-
-    function increaseQuantity(itemId) {
-        const newCart = cart.map(cartItem => {
-            if (cartItem.id === itemId && cartItem.quantity < 5) {
-                return {
-                    ...cartItem,
-                    quantity: cartItem.quantity += 1
-                }
-            }
-            return cartItem
-        })
-        setCart(newCart)
-    }
-
-    function decreaseQuantity(itemId) {
-        const newCart = cart.map(cartItem => {
-            if (cartItem.id === itemId && cartItem.quantity > 1) {
-                return {
-                    ...cartItem,
-                    quantity: cartItem.quantity -= 1
-                }
-            }
-            return cartItem
-        })
-        setCart(newCart)
-    }
+    const { data, increaseQuantity, decreaseQuantity, setCart, cart, deleteItem, addToCart } = useCart();
+    
     return (
         <>
             <Header cart={cart} deleteItem={deleteItem} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} setCart={setCart} />
